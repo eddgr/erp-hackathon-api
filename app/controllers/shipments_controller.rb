338276bf9ef5997151816ba87318e0f1c9bcc881
Edgar Ong <emailedgar@gmail.com>
2019-05-18 15:27:34 -0400

    update shipments controller params

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -15,6 +15,6 @@ class ShipmentsController < ApplicationController
   private
 
   def shipment_params
-    params.require(:shipment).permit(:company_id, :ship_to_name, :ship_to_phone, :ship_to_company_name, :ship_to_address_line1, :ship_to_address_line2, :ship_to_city_locality, :ship_to_state_province, :ship_to_postal_code, :ship_to_country_code, :ship_from_name, :ship_from_phone, :ship_from_company_name, :ship_from_address_line1, :ship_from_address_line2, :ship_from_city_locality, :ship_from_state_province, :ship_from_postal_code, :ship_from_country_code, :package_weight, :estimated_delivery)
+    params.require(:shipment).permit(:company_id, :ship_to_name, :ship_to_address, :ship_from_name, :ship_from_address, :package_weight, :estimated_delivery, :cost)
   end
 end
