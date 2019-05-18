84f3858f5691565f0ccdf173f14ff09b4d34a30d
Edgar Ong <emailedgar@gmail.com>
2019-05-18 19:30:49 -0400

    add reliability score

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,4 +1,18 @@
 class Carrier < ApplicationRecord
   has_many :shipments
   has_many :deliveries, through: :shipments
+
+  # INSTANCE METHODS
+  def sort_score
+    # self.deliveries.length === total deliveries
+    # loop through deliveries and group by -1, 0, 1
+    self.deliveries.group_by do |delivery|
+      delivery.estimated_delivery <=> delivery.actual_delivery
+    end
+    # counting all the early and on time, divide that by total to get FINAL reliability score
+  end
+
+  def reliability_score
+    ((self.deliveries.length - self.sort_score[-1].length).to_f / self.deliveries.length.to_f) * 100
+  end
 end
