ce976b67a5c39cc81b6033b86effad5e1bff0c35
Edgar Ong <emailedgar@gmail.com>
2019-05-18 14:21:35 -0400

    serialized company

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/create'
   get '/v1/shipments', to: "shipments#index"
   post '/v1/shipments/:id', to: "shipments#create"
 
   get '/v1/deliveries', to: "deliveries#index"
   post '/v1/deliveries/:id', to: "deliveries#create"
+
+  post '/v1/companies/:id', to: "companies#create"
 
   # get 'deliveries/index'
   # get 'shipments/index'
