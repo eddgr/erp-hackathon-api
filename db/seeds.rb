ab504fb0350bf88bb9353ac310f3ad087be1a072
ferryrules <ferris@ferryrules.com>
2019-05-18 19:53:38 -0400

    only 1 company

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ ship_name = Faker::Name.first_name + Faker::Name.last_name
 phone_number = Faker::PhoneNumber.phone_number
 ship_address= Faker::Address.street_address
 ship_from_name = Faker::Company.name
-ship_from_address = 
+ship_from_address =
 " #{Faker::Address.street_name} #{Faker::Address.city} #{Faker::Address.state} #{Faker::Address.zip_code}"
 random_number = rand(2..5)
 random_number2 = rand(-1..1)
@@ -21,30 +21,23 @@ random_number2 = rand(-1..1)
 
 carriers = ["USPS", "FEDEX", "DHL", 'UPS']
 
-# <----compnay seeds ---->
-50.times do 
+# <----company seeds ---->
 Company.create(name: ship_from_name)
-end 
-# <----compnay seeds ---->
+# <----company seeds ---->
 
 
 # <----Carrier Seeds --->
 carriers.each do |carrier|
 Carrier.create(name: carrier)
-end 
+end
 # <----Carrier Seeds --->
 
 # <-----Shimpment Seeds ----->
-50.times do 
-Shipment.create(company_id: rand(1..Company.all.length),carrier_id:rand(1..Carrier.all.length), ship_to_name:ship_name, ship_to_address: ship_address,
-     ship_from_name: ship_from_name, ship_from_address:ship_from_address, package_weight: random_number.to_f,
-     created_at: Date.today - random_number, estimated_delivery: Date.today - 1, cost: random_number.to_f)
+50.times do
+Shipment.create(company_id: 1, carrier_id:rand(1..Carrier.all.length), ship_to_name: ship_name, ship_to_address: ship_address, ship_from_name: ship_from_name, ship_from_address: ship_from_address, package_weight: random_number.to_f, created_at: Date.today - random_number, estimated_delivery: Date.today - 1, cost: random_number.to_f)
 end
 # <-----Shimpment Seeds ----->
 Shipment.all.each do |shipment|
      # byebug
 Delivery.create(shipment_id: shipment.id, company_id: shipment.company.id, estimated_delivery:shipment.estimated_delivery, actual_delivery: Date.today + rand(-2..0), carrier_id: shipment.carrier.id)
 end
-
-
-
