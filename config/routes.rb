0e1ee416497af2799dc841c1bc49a28f5ced1b70
Edgar Ong <emailedgar@gmail.com>
2019-05-18 14:10:16 -0400

    build read and create for deliveries and shipments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get 'deliveries/index'
-  get 'shipments/index'
-  get 'shipments/show'
-  get 'shipments/new'
-  get 'shipments/edit'
+  get '/v1/shipments', to: "shipments#index"
+  post '/v1/shipments/:id', to: "shipments#create"
+
+  get '/v1/deliveries', to: "deliveries#index"
+  post '/v1/deliveries/:id', to: "deliveries#create"
+
+  # get 'deliveries/index'
+  # get 'shipments/index'
+  # get 'shipments/show'
+  # get 'shipments/new'
+  # get 'shipments/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
