79b97d4e525aa8e0cf77ee6cfd3916b55ae9315a
Edgar Ong <emailedgar@gmail.com>
2019-05-18 18:31:04 -0400

    show estimated delivery date in delivery api

--- app/serializers/delivery_serializer.rb
+++ app/serializers/delivery_serializer.rb
@@ -1,3 +1,3 @@
 class DeliverySerializer < ActiveModel::Serializer
-  attributes :id, :actual_delivery, :shipment_id, :company_id
+  attributes :id, :actual_delivery, :shipment_id, :company_id, :estimated_delivery
 end
