c69a3a8dfcdad133c00a1c04f073a44a4e8cf724
Gamerman2001 <Tro12185@gmail.com>
2019-05-19 12:24:46 -0400

    old models old seeds

--- front-end-folder/src/carrier-audit.js
+++ front-end-folder/src/carrier-audit.js
@@ -3,3 +3,51 @@ const DELIVERIES_URL = `${BASE_URL}/v1/deliveries`
 const SHIPMENTS_URL = `${BASE_URL}/v1/shipments`
 const COMPANIES_URL = `${BASE_URL}/v1/companies`
 const CARRIERS_URL = `${BASE_URL}/v1/carriers`
+
+
+fetch(CARRIERS_URL)
+.then(function(response) {
+    return response.json();
+  }) //response start
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+    var body = document.querySelector('#body')
+    for (let i = 0; i < myJson.length; i++){
+        // for (const id of myJson)
+
+        // debugger
+        body.innerHTML += `
+        <br>
+        <br>
+        <div class= 'container'>
+            <div class= 'card'>
+            <p>carrier id : ${myJson[i].id}</p> 
+            <p>carrier name : ${myJson[i].name} </p>
+            <div>
+        <div>
+        `
+
+       
+        // debugger
+    }
+    
+
+  }); //fetch end 
+//THIS IS FOR CARRIER # 
+  fetch(CARRIERS_URL + '/1')
+.then(function(response) {
+    return response.json();
+  }) //response start
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+    var body = document.querySelector('#body')
+        // for (const id of myJson)
+        console.log(myJson)
+        // debugger
+
+        body.innerHTML += `
+        <p>${myJson}</p>
+        `
+        // debugger
+  }); //fetch end 
+  //THIS IS THE END OF CARRIER!!! 
--- front-end-folder/src/shipments.js
+++ front-end-folder/src/shipments.js
@@ -3,3 +3,33 @@ const DELIVERIES_URL = `${BASE_URL}/v1/deliveries`
 const SHIPMENTS_URL = `${BASE_URL}/v1/shipments`
 const COMPANIES_URL = `${BASE_URL}/v1/companies`
 const CARRIERS_URL = `${BASE_URL}/v1/carriers`
+
+
+
+fetch(SHIPMENTS_URL)
+.then(function(response) {
+    return response.json();
+  }) //response start
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+    var body = document.querySelector('#body')
+    for (let i = 0; i < myJson.length; i++){
+        // for (const id of myJson)
+        console.log(myJson[i].estimated_delivery)
+        // debugger
+        body.innerHTML += `
+        <br>
+        <br>
+        <div class= 'container'>
+            <div class= 'card'>
+                company id : ${myJson[i].company_id} 
+                company name : <p> "Barton-Hermann" <p>
+                estimated delivery : ${myJson[i].estimated_delivery}
+            <div>
+        <div>
+        `
+        // debugger
+    }
+    
+
+  }); //fetch end 
