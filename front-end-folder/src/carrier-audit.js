ca2d37e67e0eb9eb83ea7e567c8cfe194f4730e4
ferryrules <ferris@ferryrules.com>
2019-05-19 10:28:43 -0400

    carrier-audit

--- front-end-folder/src/carrier-audit.js
+++ front-end-folder/src/carrier-audit.js
@@ -3,3 +3,25 @@ const DELIVERIES_URL = `${BASE_URL}/v1/deliveries`
 const SHIPMENTS_URL = `${BASE_URL}/v1/shipments`
 const COMPANIES_URL = `${BASE_URL}/v1/companies`
 const CARRIERS_URL = `${BASE_URL}/v1/carriers`
+
+function grab(ele) {
+  return document.querySelector('ele')
+}
+
+
+fetch(CARRIERS_URL, {method: "GET"})
+  .then(function(resp) {
+    return resp.json()
+  })
+  .then(function(carriers) {
+    const carrierContainer = grab('#carrier-container')
+    carriers.forEach(function(carrier){
+      // debugger
+      console.log(carrier)
+      carrierContainer.innerHTML += `
+        <tr id="cname${carrier.id}">
+          <td>${carrier.name}</td>
+        </tr>
+      `
+      })
+  })
