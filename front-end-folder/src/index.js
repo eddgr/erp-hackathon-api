4d2e057e948b2b84ffa0dec2af64acbfc1d2a4bb
ferryrules <ferris@ferryrules.com>
2019-05-18 23:36:51 -0400

    frontend-ish

--- front-end-folder/src/index.js
+++ front-end-folder/src/index.js
@@ -4,14 +4,16 @@ const SHIPMENTS_URL = `${BASE_URL}/v1/shipments`
 const COMPANIES_URL = `${BASE_URL}/v1/companies`
 const CARRIERS_URL = `${BASE_URL}/v1/carriers`
 
-  function grab(selector){
-    return document.querySelector(selector)
-  };
-
+function grab(selector){
+  return document.querySelector(selector)
+};
+const company_container = grab('#company')
 fetch(COMPANIES_URL)
   .then(function(resp) {
     return resp.json()
   })
   .then(function(comp) {
-    console.log(comp);
+    company_container.innerHTML +=`
+    <p>${comp[0].name}</p>
+    `
   })
