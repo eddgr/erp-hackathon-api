f2254506420588a4d854a8232339c23585817989
ferryrules <ferris@ferryrules.com>
2019-05-18 22:19:03 -0400

    init view

--- front-end-folder/src/index.js
+++ front-end-folder/src/index.js
@@ -1,9 +1,11 @@
 const BASE_URL = "http://localhost:3000"
 const DELIVERIES_URL = `${BASE_URL}/v1/deliveries`
 const SHIPMENTS_URL = `${BASE_URL}/v1/shipments`
+const COMPANIES_URL = `${BASE_URL}/v1/companies`
+const CARRIERS_URL = `${BASE_URL}/v1/carriers`
 
   function grab(selector){
     return document.querySelector(selector)
   };
 
-const 
+const
