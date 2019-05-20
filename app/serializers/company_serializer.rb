ac1a5e9a2937dbac58bbccbda912cf60c36baec1
Edgar Ong <emailedgar@gmail.com>
2019-05-20 14:00:04 -0400

    added nested objects in api

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,3 +1,11 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id, :name, :total_deliveries, :guaranteed_deliveries, :late_deliveries
+  attributes :id, :name, :deliveries
+
+  def deliveries
+    {
+      total_deliveries: object.total_deliveries,
+      guaranteed_deliveries: object.guaranteed_deliveries,
+      late_deliveries: object.late_deliveries
+    }
+  end
 end
